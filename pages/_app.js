8666a53982cf384622893ddbb66f1c9fbe4e58c3
andriandev <andrianpoint@gmail.com>
2023-03-03 12:01:17 +0700

    Add next/font and remove eksternal fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
-import '../styles/globals.css';
 import Layout from '@/layout/main';
+import { Poppins } from 'next/font/google';
+import '../styles/globals.css';
+
+const poppins = Poppins({
+  weight: ['400', '500', '600', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <style jsx global>{`
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </Layout>
   );
